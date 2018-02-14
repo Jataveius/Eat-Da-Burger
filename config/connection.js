58d369dfe91dca5612baca3e23218041a195f499
Ashley Louitan <jataveius@aol.com>
2018-02-14 15:56:32 -0500

    update

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,12 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+var connection;
+
+if (process.env.JAWSDB_URL){
+connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else{
+
+  connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "toor",
@@ -16,4 +22,5 @@ connection.connect(function(err) {
   console.log("connected as id " + connection.threadId);
 });
 
+};
 module.exports = connection;
--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,11 @@
-DROP DATABASE burgers_db;
+-- DROP DATABASE burgers_db;
 CREATE DATABASE burgers_db;
 USE burgers_db;
 
 
 CREATE TABLE burgers
-(
-id int NOT NULL AUTO_INCREMENT,
-burger_name varchar(30) NOT NULL,
+(id int NOT NULL AUTO_INCREMENT,
+burger_name varchar (30) NOT NULL,
 devoured BOOLEAN default false,
 CreateAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 PRIMARY KEY (id)
