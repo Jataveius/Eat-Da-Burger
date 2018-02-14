a424f30b215993ed4da8fae1dcdb5964b34a9fff
Ashley Louitan <jataveius@aol.com>
2018-02-14 14:18:54 -0500

    add port

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,8 @@ var connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "toor",
-  database: "burgers_db"
+  database: "burgers_db",
+  port: "3306"
 });
 
 connection.connect(function(err) {
