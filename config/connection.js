d0071e28c9df9f536e788a4f61f07b2a42770acf
Ashley Louitan <jataveius@aol.com>
2018-02-14 01:40:55 -0500

    add route change

--- config/connection.js
+++ config/connection.js
@@ -1,40 +1,18 @@
-var mysql = require('mysql');
+var mysql = require("mysql");
 
-var connection;
-
-if (process.env.JAWSDB_URL) {
-	// DB is JawsDB on Heroku
-	connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-	// DB is local on localhost
-	connection = mysql.createConnection({
-		host: 'localhost',
-		user: 'root',
-		password: 'toor',
-		database: 'burgers_db'
-	})
-};
-// var connection = mysql.createConnection({
-//   host: "localhost",
-//   user: "root",
-//   password: "toor",
-//   database: "burgers_db"
-// });
-
-// connection.connect(function(err) {
-//   if (err) {
-//     console.error("error connecting: " + err.stack);
-//     return;
-//   }
-
-// Make the connection to MySQL
-connection.connect(function(err) {
-  if (err) {
-    console.error('ERROR: MySQL connection error -- ' + err.stack + '\n\n');
-    return;
-  }
-  console.log('Connected to MySQL database as id ' + connection.threadId + '\n\n');
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "toor",
+  database: "burgers_db"
 });
 
-// Export connection for ORM use
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
 module.exports = connection;
