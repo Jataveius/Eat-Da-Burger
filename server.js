b86527a0bb77927b61105d04b92acaae044712f4
Ashley Louitan <jataveius@aol.com>
2018-02-13 23:24:33 -0500

    routes updated

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 
-var port = 3000;
+// var port = 3000;
+var PORT = process.env.PORT || 8000;
 
 var app = express();
 
@@ -12,6 +13,8 @@ var app = express();
 app.use(express.static(process.cwd() + "/public"));
 
 app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
 
 // Override with POST having ?_method=DELETE
 app.use(methodOverride("_method"));
@@ -25,9 +28,12 @@ app.set("view engine", "handlebars");
 // Import routes and give the server access to them.
 var routes = require("./controllers/burgers_controller.js");
 
-app.use("/", routes);
+// app.use("/", routes);
+app.use(routes);
 
-app.listen(port);
 
+app.listen(PORT, function() {
+    console.log("Listening on port:%s", PORT);
+  });
 
 
